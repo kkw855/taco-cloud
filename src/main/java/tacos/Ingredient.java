d65aa5f9177fe524dad13538e1fbab6612bde63c
kkw855 <kkw855@gmail.com>
2022-12-07 12:50:02 +0900

    studying...

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,10 +1,18 @@
 package tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@SuppressWarnings("ClassCanBeRecord")
 @Data
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Entity
 public class Ingredient {
+  @Id
   private final String id;
   private final String name;
   private final Type type;
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,11 +1,36 @@
 package tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private Date createdAt;
+
+  @NotNull
+  @Size(min = 5, message = "Name must be at least 5 characters long")
   private String name;
-  private List<String> ingredients;
+  @Size(min = 1, message = "You must choose at least 1 ingredient")
+  @ManyToMany(targetEntity = Ingredient.class)
+  private List<Ingredient> ingredients;
+
+  @PrePersist
+  void createdAt() {
+    this.createdAt = new Date();
+  }
 }
 
