f27a24a7ba84afa335368e75ca3dbfbcc13406da
kkw855 <kkw855@gmail.com>
2022-12-14 17:08:29 +0900

    studying...

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,16 +21,23 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
     http
       .authorizeHttpRequests()
-      .requestMatchers("/design", "/orders")
+      .requestMatchers("/design/**", "/orders/**")
       .hasRole("USER")
       .requestMatchers("/", "/**")
       .permitAll()
       .and()
+      .csrf().disable()
       .httpBasic();
 
     return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new NoEncodingPasswordEncoder();
   }
 
   @Bean
@@ -41,24 +49,35 @@ public class SecurityConfig {
   }
 
   @Bean
-  public UserDetailsManager users(DataSource dataSource) {
-    UserDetails user1 = User.withDefaultPasswordEncoder()
-      .username("user1")
+  UserDetailsManager users(DataSource dataSource) {
+    UserDetails user = User.builder()
+      .username("user")
       .password("password1")
       .roles("USER")
       .build();
-
-    UserDetails user2 = User.withDefaultPasswordEncoder()
-      .username("user2")
-      .password("password2")
-      .roles("USER")
+    UserDetails admin = User.builder()
+      .username("admin")
+      .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+      .roles("USER", "ADMIN")
       .build();
 
     JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-
-    users.createUser(user1);
-    users.createUser(user2);
-
+    users.createUser(user);
+    users.createUser(admin);
     return users;
   }
+
+  // @Bean
+  // public UserDetailsManager users(DataSource dataSource) {
+  //   UserDetails user = User
+  //     .withUsername("user1")
+  //     .password(passwordEncoder().encode("password1"))
+  //     .roles("USER")
+  //     .build();
+  //
+  //   JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+  //   users.createUser(user);
+  //
+  //   return users;
+  // }
 }
