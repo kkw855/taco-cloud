67c36951f808603b0a5ea041ee7a0f4ffc472a7e
kkw855 <kkw855@gmail.com>
2023-01-06 16:57:05 +0900

    studying...

---
+++ src/main/java/tacos/User.java
@@ -0,0 +1,84 @@
+package tacos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Table(name = "users")
+public class User implements UserDetails {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy= GenerationType.AUTO)
+  private Long id;
+
+  private String username;
+  private String password;
+  private String fullname;
+  private String street;
+  private String city;
+  private String state;
+  private String zip;
+  private String phoneNumber;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    User user = (User) o;
+    return id != null && Objects.equals(id, user.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package tacos.security;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,27 +10,37 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.WhiteListedAllowFromStrategy;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  // @Autowired
+  // private DataSource dataSource;
+
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
     http
       .authorizeHttpRequests()
       .requestMatchers("/design/**", "/orders/**")
       .hasRole("USER")
-      .requestMatchers("/", "/**")
+      .requestMatchers("/", "/**", "/h2-console")
       .permitAll()
+      .and()
+      .headers()
+      .addHeaderWriter(
+        new XFrameOptionsHeaderWriter(
+          new WhiteListedAllowFromStrategy(Arrays.asList("localhost"))    // 여기!
+        )
+      )
+      .frameOptions().sameOrigin()
       .and()
       .csrf().disable()
       .httpBasic();
@@ -37,47 +50,41 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new NoEncodingPasswordEncoder();
+    String encodingId = "bcrypt";
+    Map<String, PasswordEncoder> encoders = new HashMap<>();
+    encoders.put(encodingId, new BCryptPasswordEncoder());
+    encoders.put("ldap", new org.springframework.security.crypto.password.LdapShaPasswordEncoder());
+    encoders.put("MD4", new org.springframework.security.crypto.password.Md4PasswordEncoder());
+    encoders.put("MD5", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("MD5"));
+    encoders.put("noop", org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance());
+    encoders.put("SHA-1", new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-1"));
+    encoders.put("SHA-256",
+      new org.springframework.security.crypto.password.MessageDigestPasswordEncoder("SHA-256"));
+    encoders.put("sha256", new org.springframework.security.crypto.password.StandardPasswordEncoder());
+    return new DelegatingPasswordEncoder(encodingId, encoders);
   }
 
   @Bean
   public DataSource dataSource() {
     return new EmbeddedDatabaseBuilder()
       .setType(EmbeddedDatabaseType.H2)
-      .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+      // .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
       .build();
   }
 
   @Bean
-  UserDetailsManager users(DataSource dataSource) {
-    UserDetails user = User.builder()
-      .username("user")
-      .password("password1")
-      .roles("USER")
-      .build();
-    UserDetails admin = User.builder()
-      .username("admin")
-      .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-      .roles("USER", "ADMIN")
-      .build();
+  JdbcUserDetailsManager users(DataSource dataSource) {
+    JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-    JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-    users.createUser(user);
-    users.createUser(admin);
-    return users;
+    return jdbcUserDetailsManager;
   }
 
-  // @Bean
-  // public UserDetailsManager users(DataSource dataSource) {
-  //   UserDetails user = User
-  //     .withUsername("user1")
-  //     .password(passwordEncoder().encode("password1"))
-  //     .roles("USER")
-  //     .build();
+  // @Autowired
+  // public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+  //   final String sqlUserName = "select u.user_name, u.user_pass, u.enable from user u where u.user_name = ?";
+  //   final String sqlAuthorities = "select ur.user_name, ur.user_role from user_role ur where ur.user_name = ?";
   //
-  //   JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-  //   users.createUser(user);
-  //
-  //   return users;
+  //   auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(sqlUserName)
+  //     .authoritiesByUsernameQuery(sqlAuthorities);
   // }
 }
